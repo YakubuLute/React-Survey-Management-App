067650a008a4d7584c0babc90cbf4de148e96e1e
Yakubu Lute <yakubulute@outlook.com>
2023-09-29 00:07:09 +0000

    Update constants.js

--- survey_client/src/utils/constants.js
+++ survey_client/src/utils/constants.js
@@ -7,6 +7,7 @@ export const CANCEL = "CANCEL";
 export const URL_ROOT = "/";
 export const URL_LOGIN = "/login";
 export const URL_CREATE = "/create";
+export const URL_LOGOUT = "/logout";
 export const URL_REGISTER = "/register";
 export const URL_SURVEY = "/survey";
 export const URL_SURVEYS = "/surveys";
@@ -15,4 +16,5 @@ export const URL_RESULTS = "/results";
 //  API
 export const API_URL_LOGIN = "http://localhost:8000/api/auth/login";
 export const API_URL_REGISTER = "http://localhost:8000/api/auth/register";
+export const API_URL_LOGOUT = "http://localhost:8000/api/auth/logout";
 export const API_URL_ROOT = "http://localhost:8000/api";
